a220f3fee38db62f9bb1150723318f226dcd335a
Paul Merrill <pdm@pdm.me>
2017-03-08 10:52:22 -0800

    Add classes for Types

---
+++ src/edu/sjsu/fwjs/Type.java
@@ -0,0 +1,91 @@
+package edu.sjsu.fwjs;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Types in FWJS.
+ * Evaluating a FWJS expression should return a FWJS value.
+ */
+public interface Type {}
+
+//NOTE: Using package access so that all implementations of Type
+//can be included in the same file.
+
+/**
+ * Boolean types.
+ */
+class BoolType implements Type {
+    @Override
+    public boolean equals(Object that) {
+        return that instanceof BoolType;
+    }
+    @Override
+    public String toString() {
+        return "Bool";
+    }
+}
+
+/**
+ * Numbers. Only integers are supported.
+ */
+class IntType implements Type {
+    @Override
+    public boolean equals(Object that) {
+        return that instanceof IntType;
+    }
+    @Override
+    public String toString() {
+        return "Int";
+    }
+}
+
+/**
+ * Strings.
+ */
+class StringType implements Type {
+    @Override
+    public boolean equals(Object that) {
+        return that instanceof StringType;
+    }
+    @Override
+    public String toString() {
+        return "String";
+    }
+}
+
+/**
+ * Type for the Unit value.
+ */
+class UnitType implements Type {
+    @Override
+    public boolean equals(Object that) {
+        return (that instanceof UnitVal);
+    }
+    @Override
+    public String toString() {
+        return "()";
+    }
+}
+
+/**
+ * A function's type. Functions take one argument and have one return value.
+ */
+class FunctionType implements Type {
+    private Type arg;
+    private Type ret;
+
+    public FunctionType(Type arg, Type ret) {
+        this.arg = arg;
+        this.ret = ret;
+    }
+    @Override
+    public boolean equals(Object that) {
+        return (that instanceof UnitVal);
+    }
+    @Override
+    public String toString() {
+        return arg + " -> " + ret;
+    }
+}
