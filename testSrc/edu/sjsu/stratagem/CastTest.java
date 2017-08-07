91ebfbcbc64edc1a14741dc8085281f561df4c45
Paul Merrill <pdm@pdm.me>
2017-08-06 17:28:00 -0700

    CastTest: Add three tests for IfExpr typechecking
    For GitHub issue #20.

--- testSrc/edu/sjsu/stratagem/CastTest.java
+++ testSrc/edu/sjsu/stratagem/CastTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.*;
 
 public class CastTest {
 
-    // Produce an int value with type Any.
-    //   fn(n: Int): ? { val }
+    // Produce an int value with type Any, as in:
+    //   fn(n: Int): ? { n }(val)
     private Expression makeAny(int val) {
         FunctionDeclExpr f = new FunctionDeclExpr(
                 "n",
@@ -19,9 +19,10 @@ public class CastTest {
     }
 
     @Test
-    // let n: ? = 1
-    // in fn(s: String): Unit { unit }
-    //  throws StratagemCastException
+    // Assert that
+    //   let n: ? = 1 in
+    //   fn(s: String): Unit { unit }
+    // throws a StratagemCastException
     public void testApplicationCastException() {
         Expression n = makeAny(1);
         FunctionDeclExpr f = new FunctionDeclExpr(
@@ -39,5 +40,30 @@ public class CastTest {
             return;  // pass
         }
         assertTrue("Failed to throw StratagemCastException", false);
+    }
+
+    @Test
+    // Assert that
+    //   if (true) { 1 } else { unit }
+    // has type ?
+    public void testPrimitiveIfCast() {
+    }
+
+    @Test
+    // Assert that
+    //   let identity: ? -> ? = fn(x: ?): ? { x } in
+    //   let succ: Int -> Int = fn(n: Int): Int { n + 1 } in
+    //   if (true) { identity } else { succ }
+    // has type Int -> ?
+    public void testSimpleFunctionIfCast() {
+    }
+
+    @Test
+    // Assert that
+    //   let fn1: String -> Int -> ? = ... in
+    //   let fn2: Unit -> Int -> Int = ... in
+    //   if (true) { fn1 } else { fn2 }
+    // has type ? -> Int -> Int
+    public void testComplexFunctionIfCast() {
     }
 }
