d604421274d2e398f1f646a492b00a65fb6395a2
Paul Merrill <pdm@pdm.me>
2017-08-07 17:30:26 -0700

    CastTest: Add four more simple function tests

--- testSrc/edu/sjsu/stratagem/CastTest.java
+++ testSrc/edu/sjsu/stratagem/CastTest.java
@@ -31,7 +31,7 @@ public class CastTest {
     // Assert that
     //   if (true) { true } else { unit }
     // has type ?
-    public void testPrimitiveIfCast() {
+    public void testPrimitiveIfCast1() {
         IfExpr ifExpr = new IfExpr(
                 ValueExpr.trueSingleton,
                 ValueExpr.trueSingleton,
@@ -44,17 +44,104 @@ public class CastTest {
 
     @Test
     // Assert that
+    //   if (true) { true } else { <?>unit }
+    // has type ?
+    public void testPrimitiveIfCast2() {
+        IfExpr ifExpr = new IfExpr(
+                ValueExpr.trueSingleton,
+                ValueExpr.trueSingleton,
+                TestUtils.makeAny(UnitVal.singleton));
+
+        Type ifResultType = ifExpr.typecheck(new TypeEnvironment());
+
+        assertEquals(AnyType.singleton, ifResultType);
+    }
+
+    @Test
+    // Assert that
+    //   let fn1: Int -> Unit = ... in
+    //   let fn2: Unit -> Unit = ... in
+    //   if (true) { fn1 } else { fn2 }
+    // has type ? -> Unit
+    public void testSimpleFunctionIfCast1() {
+        Expression fn1 = TestUtils.makeTrivialFn(IntType.singleton, UnitType.singleton);
+        Expression fn2 = TestUtils.makeTrivialFn(UnitType.singleton, UnitType.singleton);
+
+        IfExpr ifExpr = new IfExpr(ValueExpr.trueSingleton, fn1, fn2);
+
+        Type ifResultType = ifExpr.typecheck(new TypeEnvironment());
+        Type anyToUnit = new ClosureType(AnyType.singleton, UnitType.singleton);
+
+        assertEquals(anyToUnit, ifResultType);
+    }
+
+    @Test
+    // Assert that
+    //   let fn1: ? -> Unit = ... in
+    //   let fn2: Unit -> Unit = ... in
+    //   if (true) { fn1 } else { fn2 }
+    // has type ? -> Unit
+    public void testSimpleFunctionIfCast2() {
+        Expression fn1 = TestUtils.makeTrivialFn(AnyType.singleton, UnitType.singleton);
+        Expression fn2 = TestUtils.makeTrivialFn(UnitType.singleton, UnitType.singleton);
+
+        IfExpr ifExpr = new IfExpr(ValueExpr.trueSingleton, fn1, fn2);
+
+        Type ifResultType = ifExpr.typecheck(new TypeEnvironment());
+        Type anyToUnit = new ClosureType(AnyType.singleton, UnitType.singleton);
+
+        assertEquals(anyToUnit, ifResultType);
+    }
+
+    @Test
+    // Assert that
+    //   let fn1: Unit -> Int = ... in
+    //   let fn2: Unit -> Unit = ... in
+    //   if (true) { fn1 } else { fn2 }
+    // has type Unit -> ?
+    public void testSimpleFunctionIfCast3() {
+        Expression fn1 = TestUtils.makeTrivialFn(UnitType.singleton, IntType.singleton);
+        Expression fn2 = TestUtils.makeTrivialFn(UnitType.singleton, UnitType.singleton);
+
+        IfExpr ifExpr = new IfExpr(ValueExpr.trueSingleton, fn1, fn2);
+
+        Type ifResultType = ifExpr.typecheck(new TypeEnvironment());
+        Type unitToAny = new ClosureType(UnitType.singleton, AnyType.singleton);
+
+        assertEquals(unitToAny, ifResultType);
+    }
+
+    @Test
+    // Assert that
+    //   let fn1: Unit -> ? = ... in
+    //   let fn2: Unit -> Int = ... in
+    //   if (true) { fn1 } else { fn2 }
+    // has type Unit -> ?
+    public void testSimpleFunctionIfCast4() {
+        Expression fn1 = TestUtils.makeTrivialFn(UnitType.singleton, AnyType.singleton);
+        Expression fn2 = TestUtils.makeTrivialFn(UnitType.singleton, UnitType.singleton);
+
+        IfExpr ifExpr = new IfExpr(ValueExpr.trueSingleton, fn1, fn2);
+
+        Type ifResultType = ifExpr.typecheck(new TypeEnvironment());
+        Type unitToAny = new ClosureType(UnitType.singleton, AnyType.singleton);
+
+        assertEquals(unitToAny, ifResultType);
+    }
+
+    @Test
+    // Assert that
     //   let identity: ? -> ? = fn(x: ?): ? { x } in
     //   let succ: Int -> Int = fn(n: Int): Int { n + 1 } in
     //   if (true) { identity } else { succ }
-    // has type Int -> ?
-    public void testSimpleFunctionIfCast() {
+    // has type ? -> ?
+    public void testSimpleFunctionIfCast5() {
         IfExpr ifExpr = new IfExpr(ValueExpr.trueSingleton, TestUtils.id, TestUtils.succ);
 
         Type ifResultType = ifExpr.typecheck(new TypeEnvironment());
-        Type intToAny = new ClosureType(IntType.singleton, AnyType.singleton);
+        Type anyToAny = new ClosureType(AnyType.singleton, AnyType.singleton);
 
-        assertEquals(intToAny, ifResultType);
+        assertEquals(anyToAny, ifResultType);
     }
 
     @Test
@@ -62,7 +149,7 @@ public class CastTest {
     //   let fn1: String -> Int -> ? = ... in
     //   let fn2: Unit -> Int -> Int = ... in
     //   if (true) { fn1 } else { fn2 }
-    // has type ? -> Int -> Int
+    // has type ? -> Int -> ?
     public void testComplexFunctionIfCast() {
         Expression fn1 = TestUtils.makeTrivialFn(StringType.singleton, IntType.singleton, AnyType.singleton);
         Expression fn2 = TestUtils.makeTrivialFn(UnitType.singleton, IntType.singleton, IntType.singleton);
@@ -70,12 +157,12 @@ public class CastTest {
         IfExpr ifExpr = new IfExpr(ValueExpr.trueSingleton, fn1, fn2);
 
         Type ifResultType = ifExpr.typecheck(new TypeEnvironment());
-        Type anyToIntToInt = new ClosureType(
+        Type anyToIntToAny = new ClosureType(
                 AnyType.singleton,
                 new ClosureType(
                         IntType.singleton,
-                        IntType.singleton));
+                        AnyType.singleton));
 
-        assertEquals(anyToIntToInt, ifResultType);
+        assertEquals(anyToIntToAny, ifResultType);
     }
 }
